2d586304c4c0bf8fa6d24619396b933a6d514026
haneuma0628 <haneuma0628@gmail.com>
2013-09-18 20:46:31 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function ()
+  {
+      var cookieSize = 64;
+      var i = 0, j = 1, k = 1;;
+      var count = setInterval(fall, 50);
+      $('#screen').mousedown(function (event)
+			 {
+			     var rnd = Math.floor(Math.random()*4);
+			     $('#cookie').append('<img src = "img/cookie'+rnd+'.png" class="cookie'+i+'"/>');
+			     $('.cookie'+i).css('top', event.offsetY);
+			     $('.cookie'+i).css('left', event.offsetX);
+			     i+=1;
+			 }
+			);
+      function fall()
+      {
+	  $('img').each(function (){
+	      var rndX = Math.floor(Math.random()*16) - Math.floor(Math.random()*16), rndY = Math.floor(Math.random()*16) - Math.floor(Math.random()*16);
+	      var elem = $(this);
+	      var position = elem.offset();
+	      if (position.top > 0 && position.left > 0 && position.top < 1000-cookieSize && position.left < 1000-cookieSize) {
+		  elem.css('top', position.top + rndY);
+		  elem.css('left', position.left + rndX);
+	      }
+	  });
+      }
+  });
+/*
+var cookieSize = 64;
+
+function bake()
+{
+    var x = event.x, y = event.y;
+    cookie.style.posTop = y - cookieSize/2;
+    cookie.style.posLeft = x - cookieSize/2;
+}*/
